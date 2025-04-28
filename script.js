19e4566df3def9130ac2dc2f64016d9e5ea5e8a5
duduzye <edu.oli.games@gmail.com>
2025-04-27 23:52:54 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -14,3 +14,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
     items.forEach(item => observer.observe(item));
 });
+
+const themeToggleButton = document.getElementById('theme-toggle');
+const body = document.body;
+
+themeToggleButton.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+    if (body.classList.contains('dark-mode')) {
+        themeToggleButton.textContent = 'Modo Claro';
+    } else {
+        themeToggleButton.textContent = 'Modo Escuro';
+    }
+});
